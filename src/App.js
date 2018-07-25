0716ec82086354200503e6e1560439f81e021245
James George <jamesgeorge998001@gmail.com>
2018-07-25 13:09:16 +0530

    Add proper data sets and define charts:tada:

--- src/App.js
+++ src/App.js
@@ -1,66 +1,83 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-unfetch';
-import LineChart from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import './App.css';
+
+const options = {
+  scaleShowGridLines: true,
+  scaleGridLineColor: "rgba(0,0,0,.05)",
+  scaleGridLineWidth: 1,
+  scaleShowHorizontalLines: true,
+  scaleShowVerticalLines: true,
+  bezierCurve: true,
+  bezierCurveTension: 0.4,
+  pointDot: true,
+  pointDotRadius: 4,
+  pointDotStrokeWidth: 1,
+  pointHitDetectionRadius: 20,
+  datasetStroke: true,
+  datasetStrokeWidth: 2,
+  datasetFill: true,
+  legendTemplate:
+    '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
+};
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {cryptoCoinData : []};
+    this.state = {coinData: this.getCoinData()};
     this.getCoinData = this.getCoinData.bind(this);
-    this.coinData ={ labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    }
-  }
-  componentWillMount(){
-    this.getCoinData();
   }
   getCoinData = () => {
     let count = 0;
-    let coinData = []
+    let cryptoCoinData = []
+    let coinNames = [];
+    let coinPrices = [];
     fetch('https://api.coinmarketcap.com/v2/ticker/')
     .then(response => response.json())
     .then(coinList => {
       for (let key in coinList.data){
         if(count === 10) break;
         console.log(coinList.data[key]);
-        coinData.push(coinList.data[key]);
-        count++;
+        if(coinList.data[key].quotes.USD.price > 100){
+            cryptoCoinData.push(coinList.data[key]);
+            count++;
+          }
         }
-        //this.setState({cryptoCoinData: coinData});
-        console.log(coinData);
+        console.log(cryptoCoinData); 
+        cryptoCoinData.map(coin => {
+          coinNames.push(coin.name);
+          coinPrices.push(coin.quotes.USD.price);
+          });
+        console.log(coinNames);
+        console.log(coinPrices);
         
-        console.log('State:\n' + this.state.cryptoCoinData);
         
       }
     )
     .catch(err => console.log(err));
+
+    return {
+      labels: coinNames,
+      datasets: [
+        {
+          label: 'Dataset',
+          fillColor: 'rgba(220,220,220,0.2)',
+          strokeColor: 'rgba(220,220,220,1)',
+          pointColor: 'rgba(220,220,220,1)',
+          pointStrokeColor: '#fff',
+          pointHighlightFill: '#fff',
+          pointHighlightStroke: 'rgba(220,220,220,1)',
+          data: coinPrices,
+        },
+      ]
+    }
     
   }
   render() {
     return (
       <div>
-        <LineChart data={this.coinData} />
+        <Line data={this.state.coinData} options={options} width={600} height={250} />
       </div>
     );
   }
