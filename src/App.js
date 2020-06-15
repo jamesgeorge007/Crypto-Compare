afdb602a82ae43077cce3afd7a966f381609c451
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-06-15 13:59:30 +0530

    fix: regression

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import fetch from 'isomorphic-unfetch';
-import { Line, Bar, Doughnut, Pie, HorizontalBar } from 'react-chartjs-2';
-import './App.css';
+import React, { Component } from "react";
+import fetch from "isomorphic-unfetch";
+import { Line, Bar, Doughnut, Pie, HorizontalBar } from "react-chartjs-2";
+import "./App.css";
 
 const options = {
   scaleShowGridLines: true,
@@ -21,47 +21,40 @@ const options = {
 };
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {coinData: this.getCoinData(), chart: 'Line'};
+    this.state = { coinData: this.getCoinData(), chart: "Line" };
     this.getCoinData = this.getCoinData.bind(this);
   }
   getCoinData = () => {
     let count = 0;
-    let cryptoCoinData = []
+    let cryptoCoinData = [];
     let coinNames = [];
     let coinPrices = [];
-    fetch('https://api.coinmarketcap.com/v2/ticker/')
-    .then(response => response.json())
-    .then(coinList => {
-      for (let key in coinList.data){
-        if(count === 10) break;
-        console.log(coinList.data[key]);
-        if(coinList.data[key].quotes.USD.price > 100){
+    fetch("https://api.coinmarketcap.com/v2/ticker/")
+      .then((response) => response.json())
+      .then((coinList) => {
+        for (let key in coinList.data) {
+          if (count === 10) break;
+          if (coinList.data[key].quotes.USD.price > 100) {
             cryptoCoinData.push(coinList.data[key]);
             count++;
           }
         }
-        console.log(cryptoCoinData); 
-        cryptoCoinData.map(coin => {
+        cryptoCoinData.forEach((coin) => {
           coinNames.push(coin.name);
           coinPrices.push(coin.quotes.USD.price);
-          });
-        console.log(coinNames);
-        console.log(coinPrices);
-        
-        
-      }
-    )
-    .catch(err => console.log(err));
+        });
+      })
+      .catch((err) => console.error(err));
 
     return {
       labels: coinNames,
       datasets: [
         {
-          label: 'Dataset',
+          label: "Dataset",
           data: coinPrices,
-          backgroundColor: [                
+          backgroundColor: [
             "#79CAF2",
             "#80DEEA",
             "#A5D6A7",
@@ -71,7 +64,7 @@ class App extends Component {
             "#C5E1A5",
             "#FFF59D",
           ],
-          hoverBackgroundColor: [            
+          hoverBackgroundColor: [
             "#31B2F2",
             "#00BCD4",
             "#4CAF50",
@@ -80,32 +73,51 @@ class App extends Component {
             "#4CAF50",
             "#8BC34A",
             "#FFEB3B",
-          ]
+          ],
         },
-      ]
-    }
-    
-  }
+      ],
+    };
+  };
 
   render() {
-    let comparisonChart;
-    if(this.state.chart === 'Line'){
-       comparisonChart = <Line data={this.state.coinData} redraw options={options} width={600} height={250} />;
-    } else if(this.state.chart === 'Bar'){
-       comparisonChart = <Bar data={this.state.coinData} redraw options={options} width={600} height={250} />;
-    } else if (this.state.chart === 'Horizontal-Bar') {
-      comparisonChart = <HorizontalBar data={this.state.coinData} redraw options={options} width={600} height={250} />;
-    } else if (this.state.chart === 'Doughnut') {
-      comparisonChart = <Doughnut data={this.state.coinData} redraw options={options} width={600} height={250} />;
-    } else if (this.state.chart === 'Pie') {
-      comparisonChart = <Pie data={this.state.coinData} redraw options={options} width={600} height={250} />;
-    }
+    const renderProps = {
+      data: this.state.coinData,
+      redraw: true,
+      options,
+      width: 600,
+      height: 250,
+    };
+    const mapChoiceToComponent = {
+      Line: Line,
+      Bar: Bar,
+      Doughnut: Doughnut,
+      Pie: Pie,
+    };
+    const ComparisonChart = this.state.chart.includes("-")
+      ? HorizontalBar
+      : mapChoiceToComponent[this.state.chart];
     return (
       <div>
-        <div style={{padding: 10, borderWidth: 10, borderColor: '#ddd', borderStyle: 'solid'}}>
-        {comparisonChart} 
+        <div
+          style={{
+            padding: 10,
+            borderWidth: 10,
+            borderColor: "#ddd",
+            borderStyle: "solid",
+          }}
+        >
+          <ComparisonChart {...renderProps} />
         </div>
-        <select style={{width: '80vw', margin: '10vh auto', borderColor: '#2e2e2e'}} className="form-control" onChange={e => this.setState({coinData: this.getCoinData(), chart: e.target.value})}>
+        <select
+          style={{ width: "80vw", margin: "10vh auto", borderColor: "#2e2e2e" }}
+          className="form-control"
+          onChange={(e) =>
+            this.setState({
+              coinData: this.getCoinData(),
+              chart: e.target.value,
+            })
+          }
+        >
           <option value="Line">Line-Chart</option>
           <option value="Bar">Bar-Chart</option>
           <option value="Horizontal-Bar">Horizontal-Bar-Chart</option>
